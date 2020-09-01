2c2ece3e11cf2d2ebeecb575cd45ab138962adf1
HelpinGTech <70471846+HelpinGTech@users.noreply.github.com>
2020-09-01 12:08:10 +0545

    Add files via upload

---
+++ madlibsgenerator.py
@@ -0,0 +1,9 @@
+adj1 = input("Enter an adjective and press Enter: ")
+noun1 = input("Enter an noun (plural) and press Enter: ")
+noun2 = input("Enter another noun and press Enter: ")
+adj2 = input("Enter another adjective and press Enter: ")
+
+print ("Roses are " + adj1)
+print (noun1 + " are blue")
+print (noun2 + " is " + adj2 )
+print ("And so are you!")
